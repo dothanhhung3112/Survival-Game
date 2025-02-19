793d7cc09a8e1b52e0033d3e9204284a1ca04046
hungsilver <dothanhhung3112@gmail.com>
2025-02-19 23:27:56 +0700

    .

--- Assets/levels/Marbleshoot/Scripts/Marble.cs
+++ Assets/levels/Marbleshoot/Scripts/Marble.cs
@@ -3,31 +3,40 @@
 public class Marble : MonoBehaviour
 {
     Rigidbody rb;
-    public float forceMultiplier = 10f; 
-    public float angle = 30f; 
+    public float forceMultiplier = 10f;
+    public float angle = 30f;
     private Vector2 startTouchPos;
     private Vector2 endTouchPos;
     private bool isDragging = false;
 
+    [SerializeField] TrailRenderer trail;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) 
+        if (Input.GetMouseButtonDown(0))
         {
             startTouchPos = Input.mousePosition;
             isDragging = true;
         }
-        else if (Input.GetMouseButtonUp(0) && isDragging) // Khi thả ngón tay
+        else if (Input.GetMouseButtonUp(0) && isDragging)
         {
             endTouchPos = Input.mousePosition;
             isDragging = false;
 
-            Vector2 dragVector = endTouchPos - startTouchPos; // Lấy hướng vuốt
-            float dragDistance = dragVector.magnitude; // Khoảng cách vuốt
-            float forceAmount = dragDistance * forceMultiplier * 0.01f; // Chuyển khoảng cách thành lực
+            Vector2 dragVector = endTouchPos - startTouchPos;
+            float dragDistance = dragVector.magnitude;
+            float forceAmount = dragDistance * forceMultiplier * 0.01f;
 
-            // Chỉ lấy lực theo trục Z và Y (bỏ X để không lệch hướng)
-            Vector3 throwDirection = Quaternion.Euler(-angle, 0, 0) * Vector3.forward; // Ném theo góc chéo
-            rb.AddForce(throwDirection * forceAmount, ForceMode.Impulse); // Bắn object
+            float angleY = Mathf.Atan2(dragVector.x, dragVector.y) * Mathf.Rad2Deg;
+            Vector3 throwDirection = Quaternion.Euler(-angle, angleY, 0) * Vector3.forward;
+            rb.isKinematic = false;
+            Physics.gravity = new Vector3(0, -3f, 0);
+            rb.AddForce(throwDirection * forceAmount, ForceMode.Impulse);
         }
     }
 }
