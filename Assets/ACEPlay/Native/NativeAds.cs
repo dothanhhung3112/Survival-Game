3c7fda1367629717cf1d35f342a11b64df46f3eb
Do Thanh Hung <dothanhhung3112@gmail.com>
2025-03-04 18:14:22 +0700

    .

---
+++ Assets/ACEPlay/Native/NativeAds.cs
@@ -0,0 +1,43 @@
+using ACEPlay.Bridge;
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ACEPlay.Native
+{
+    public class NativeAds : MonoBehaviour
+    {
+        public static NativeAds instance;
+#if UNITY_ANDROID
+        [SerializeField] string adUnitId = "ca-app-pub-6632644511740977/6806026708";
+        string idTest = "ca-app-pub-3940256099942544/2247696110";
+#elif UNITY_IOS
+        string adUnitId = "ca-app-pub-6632644511740977/2493351992";
+        string idTest = "ca-app-pub-3940256099942544/3986624511";
+#endif
+
+        [Header("UI Native")]
+        [SerializeField] private bool isIconNative;
+        [SerializeField] private GameObject nativeAds;
+        [SerializeField] private RawImage rawMainImage, rawIconImage;
+        [SerializeField] private TextMeshProUGUI txtTitle;
+        [SerializeField] private TextMeshProUGUI txtDescription;
+        [SerializeField] private TextMeshProUGUI txtButton;
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            nativeAds.SetActive(false);
+        }
+
+        public void DisplayNativeAds(bool enable)
+        {
+            nativeAds.SetActive(false);
+        }
+    }
+}
